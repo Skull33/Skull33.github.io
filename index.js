c1e322ebfc61d4969a447a81e103b8c478ad9caa
Skull33 <alonzo.cuela@ucsp.edu.pe>
2024-05-01 16:47:18 -0500

    pagina SS v0.02 update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 //obtenemos el texto por una ID
 var titulo = document.getElementById('titulopagina')
+var titulo1 = document.getElementById('titulopagina1')
+var titulo2 = document.getElementById('titulopagina2')
 //obtenemos el titulo ya identificado
 var tituloinden = titulo.innerText;
+var tituloinden1 = titulo1.innerText;
+var tituloinden2 = titulo2.innerText;
 //creamos un nuevo texto que guardara la mayusculas
 var nuevotexto = '';
+var nuevotexto1 = '';
+var nuevotexto2 = '';
 //procedemos con la recoleccion r¿de mayuculas
 for(var i = 0; i < tituloinden.length; i++)
 {
@@ -11,7 +17,7 @@ for(var i = 0; i < tituloinden.length; i++)
     if(tituloinden[i] === tituloinden[i].toUpperCase() && tituloinden[i] !== tituloinden[i].toLowerCase())
     {
         //convertimos las letras mayusculas a otro color
-        nuevotexto += '<span style = "color:red;">' + tituloinden[i] + "</span>";
+        nuevotexto += '<span style = "color:black;">' + tituloinden[i] + "</span>";
     }
     else
     {
@@ -19,4 +25,34 @@ for(var i = 0; i < tituloinden.length; i++)
     }
 
     titulo.innerHTML = nuevotexto;
+}
+for(var o = 0; o < tituloinden1.length; o++)
+{
+    //verificamos si la letra en si es mayuscula
+    if(tituloinden1[o] === tituloinden1[o].toUpperCase() && tituloinden1[o] !== tituloinden1[o].toLowerCase())
+    {
+        //convertimos las letras mayusculas a otro color
+        nuevotexto1 += '<span style = "color:red;">' + tituloinden1[o] + "</span>";
+    }
+    else
+    {
+        nuevotexto1 += tituloinden1[o];
+    }
+
+    titulo1.innerHTML = nuevotexto1;
+}
+for(var j = 0; j < tituloinden2.length; j++)
+{
+    //verificamos si la letra en si es mayuscula
+    if(tituloinden2[j] === tituloinden2[j].toUpperCase() && tituloinden2[j] !== tituloinden2[j].toLowerCase())
+    {
+        //convertimos las letras mayusculas a otro color
+        nuevotexto2 += '<span style = "color:green;">' + tituloinden2[j] + "</span>";
+    }
+    else
+    {
+        nuevotexto2 += tituloinden2[j];
+    }
+
+    titulo2.innerHTML = nuevotexto2;
 }
