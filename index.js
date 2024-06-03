57034aa795e3b604b100f4ee0e91ce784cfac96f
Skull33 <alonzo.cuela@ucsp.edu.pe>
2024-06-03 16:59:36 -0500

    paginaSS v0.05

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
-//obtenemos el texto por una ID
-var titulo = document.getElementById('titulopagina');
-var titulo1 = document.getElementById('titulopagina1');
-var titulo1_1 = document.getElementById('titulopagina1_1');
-//obtenemos el titulo ya identificado
-var tituloinden = titulo.innerText;
-var tituloinden1 = titulo1.innerText;
-var tituloinden1_1 = titulo1_1.innerText;
-//creamos un nuevo texto que guardara la mayusculas
-var nuevotexto = '';
-var nuevotexto1 = '';
-var nuevotexto1_1 = '';
-//procedemos con la recoleccion r¿de mayuculas
-for(var i = 0; i < tituloinden.length; i++)
+window.addEventListener('load', function()
 {
-    //verificamos si la letra en si es mayuscula
-    if(tituloinden[i] === tituloinden[i].toUpperCase() && tituloinden[i] !== tituloinden[i].toLowerCase())
+    var elementos = document.querySelectorAll('.Mayusculas');
+    var elementos1 = document.querySelectorAll('.Mayusculas1');
+    var elementos2 = document.querySelectorAll('.Mayusculas2');
+    elementos.forEach(function(elemento)
     {
-        //convertimos las letras mayusculas a otro color
-        nuevotexto += '<span style = "color:black;">' + tituloinden[i] + "</span>";
-    }
-    else
-    {
-        nuevotexto += tituloinden[i];
-    }
-
-    titulo.innerHTML = nuevotexto;
-}
-for(var o = 0; o < tituloinden1.length; o++)
-{
-    //verificamos si la letra en si es mayuscula
-    if(tituloinden1[o] === tituloinden1[o].toUpperCase() && tituloinden1[o] !== tituloinden1[o].toLowerCase())
-    {
-        //convertimos las letras mayusculas a otro color
-        nuevotexto1 += '<span style = "color:red;">' + tituloinden1[o] + "</span>";
-    }
-    else
-    {
-        nuevotexto1 += tituloinden1[o];
-    }
-    titulo1.innerHTML = nuevotexto1;    
-}
-for(var u = 0; u < tituloinden1.length; u++)
-    {
-        //verificamos si la letra en si es mayuscula
-        if(tituloinden1_1[u] === tituloinden1_1[u].toUpperCase() && tituloinden1_1[u] !== tituloinden1_1[u].toLowerCase())
+        elemento.innerHTML = elemento.innerHTML.replace(/[A-ZÁÉÍÓÚÜÑ]/g, function(match)
         {
-            //convertimos las letras mayusculas a otro color
-            nuevotexto1_1 += '<span style = "color:red;">' + tituloinden1_1[u] + "</span>";
-        }
-        else
+            return '<span style="color: black;">' + match + '</span>';
+        });
+    });
+    elementos1.forEach(function(elemento1)
+    {
+        elemento1.innerHTML = elemento1.innerHTML.replace(/[A-ZÁÉÍÓÚÜÑ]/g, function(match)
         {
-            nuevotexto1_1 += tituloinden1_1[u];
-        }
-        titulo1_1.innerHTML = nuevotexto1_1;    
-}
+            return '<span style="color: green;">' + match + '</span>';
+        });
+    });
+    elementos2.forEach(function(elemento2)
+    {
+        elemento2.innerHTML = elemento2.innerHTML.replace(/[A-ZÁÉÍÓÚÜÑ]/g, function(match)
+        {
+            return '<span style="color: red;">' + match + '</span>';
+        });
+    });
+});
